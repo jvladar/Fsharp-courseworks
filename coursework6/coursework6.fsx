a3493d8f6a5a8ae76b6e8e1693fa5b7d5eac4cc6
jvladar <jvladar@centrum.sk>
2021-11-03 18:33:15 +0200

    6.0

--- coursework6/coursework6.fsx
+++ coursework6/coursework6.fsx
@@ -74,20 +74,18 @@ let rec pHoldsForAllSequentialElements (predicate: int->int->bool) (xs: int list
   Make sure your implementation uses explicit tail recursion.
 *)
 
-let createTwoTouplesOfList (cislo : 'a) (pole: 'a list) =
+let createTwoTuplesOfList (cislo : 'a) (pole: 'a list) =
   let updPole = if (pole.Length % 2 <> 0) then pole @ [cislo] else pole
 
-  let rec createTwoTouplesOfLists2 pole pole2 = 
+  let rec createTwoTuplesOfLists2 pole pole2 = 
     match pole with
-    | x::y::rest -> createTwoTouplesOfLists2 rest ((x,y)::pole2)
-    | _ -> List.rev(pole2)
-    
-  createTwoTouplesOfLists2 updPole []
-  //createTwoTouplesOfLists2 updPole []
+    | x::y::rest -> createTwoTuplesOfLists2 rest ((x,y)::pole2)
+    | _ -> List.rev(pole2) 
+  createTwoTuplesOfLists2 updPole []
 
-  
 
-//createTwoTouplesOfLists 1 [2]
+
+//createTwoTouplesOfList 1 [2;4;5]
 
 (*
   Task 3:
